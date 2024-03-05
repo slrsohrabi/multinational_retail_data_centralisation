cd9d0790fa3e543412dfb08783c703745970dcb3
Salar <salarsohrabi@yahoo.com>
2024-03-05 16:08:11 +0000

    Added new files to the repository

---
+++ data_cleaning.py
@@ -0,0 +1,4 @@
+#Methods to clean data from each of the data sources.
+class DataCleaning:
+    def __init__(self) -> None:
+        pass
---
+++ database_utils.py
@@ -0,0 +1,4 @@
+#Will use to connect with and upload data to the database.
+class DatabaseConnector:
+    def __init__(self) -> None:
+        pass
