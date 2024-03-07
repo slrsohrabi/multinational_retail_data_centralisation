d93a6ba66ac2aba69c37c69a31a39dcd9b68da5d
Salar <salarsohrabi@yahoo.com>
2024-03-07 23:38:33 +0000

    data extraction complete

--- data_extraction.py
+++ data_extraction.py
@@ -30,4 +30,29 @@ class DataExtractor:
         df = pd.DataFrame(ext_data,columns = columns)
         return df
 
+import pandas as pd
+
+class DataExtractor:
+    def __init__(self, engine):
+        self.engine = engine
+
+    # Method to list the columns of a given table in the RDS database.
+    def list_db_tables(self, table_name):
+        with self.engine.connect() as conn:
+            query = f"SELECT * FROM {table_name}"
+            result = conn.execute(query)
+            data = result.fetchall()
+            # columns = result.keys()
+        return data
+    
+    # Method to extract the database table to a pandas DataFrame.
+    def read_rds_table(self, table_name):
+        # Get the names of all tables in the database
+        # tables = self.list_db_tables(table_name)
+        # if table_name not in tables:
+        #     raise ValueError(f"Table '{table_name}' not found in the database.")
+        # Extract the table containing user data and return a pandas DataFrame.
+        # ext_data, columns = tables
+        df = pd.read_sql_table(table_name,self.engine)
+        return df
 
--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 from database_utils import DatabaseConnector
 from data_extraction import DataExtractor
-from pandasgui import show
-
+# from pandasgui import show
 def main():
-    # Initialize DatabaseConnector
+    #initialize DatabaseConnector
     db_connector = DatabaseConnector()
 
-    # Read database credentials from YAML file and initialize database engine
+    #read database credentials from YAML file and initialize database engine
     cred_data = db_connector.read_db_creds()
     engine = db_connector.init_db_engine(cred_data)
-
-    # Initialize DataExtractor with the database engine
+    print(engine)
+    
+    #initialize DataExtractor 
     extractor = DataExtractor(engine)
 
-    # Step 1: Get the names of all tables in the database
+    #get the names of all tables in the database
     tables = db_connector.list_db_tables(engine)
 
-    # Step 2: Create an empty dictionary to store table data
-    table_data = {}
-
-    # Step 3: Loop through each table and extract data into a DataFrame
+    #loop through each table and extract data into a DataFrame
     for table_name in tables:
-        table_data[table_name] = extractor.read_rds_table(db_connector, table_name)
-
-    # Step 4: Display all tables using PandasGUI
-    show(table_data)
+       df = extractor.read_rds_table(table_name)
+       df.to_csv(table_name+'.csv')
 
 if __name__ == "__main__":
     main()
