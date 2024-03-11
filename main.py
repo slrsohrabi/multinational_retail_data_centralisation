86e7bdfdff7f43d49edf241371bb068c93ac541e
Salar <salarsohrabi@yahoo.com>
2024-03-11 23:40:01 +0000

    data extraction complete/Users/mac/opt/miniconda3/envs/mrdc_env/bin/python

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from database_utils import DatabaseConnector
-from data_extraction import DataExtractor
+# from database_utils import DatabaseConnector
+# from data_extraction import DataExtractor
+from data_cleaning import DataCleaning
+import pandas as pd
 # from pandasgui import show
 def main():
-    #initialize DatabaseConnector
+    """ #initialize DatabaseConnector
     db_connector = DatabaseConnector()
 
     #read database credentials from YAML file and initialize database engine
@@ -19,7 +21,18 @@ def main():
     #loop through each table and extract data into a DataFrame
     for table_name in tables:
        df = extractor.read_rds_table(table_name)
-       df.to_csv(table_name+'.csv')
+       df.to_csv(table_name+'.csv') """
+    
+    #
+
+    data_cleaner = DataCleaning()
+    df = pd.read_csv('legacy_users.csv')
+    df = data_cleaner.clean_user_data(df)
+    df = df.set_index('index')
+    df.to_csv('legacy_users.csv')
+   
+    """ df = pd.read_csv('legacy_users.csv')
+    print(df['country'].value_counts()) """
 
 if __name__ == "__main__":
     main()
